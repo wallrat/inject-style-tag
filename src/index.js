a5141342388c1aa81d21858d160e12237ed85fad
Andreas Bielk <andreas.bielk@six-group.se>
2016-12-13 13:11:35 +0100

    remove debug logging

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ console.log('inject-style-tag loaded!!.')
 
 let lastIdInserted = null
 
-// Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
-// tags it will allow on a page
+// Force single-tag solution on IE6-9, which has a hard limit
+// on the # of <style> tags it will allow on a page
 let createSingleTag = /msie [6-9]\b/.test(self.navigator.userAgent.toLowerCase())
 
 module.exports = {
   inject(css,id) {
-    console.log('inject',css,id)
 
-    // check if css is already injected exists
+    // check if css is already injected
     if (document.getElementById(id)) {
-      console.log('css already added')
       return
     }
 
@@ -30,12 +28,9 @@ module.exports = {
 
     if (lastStyleInserted) {
         if (createSingleTag) {
-          console.log('handling IE9')
           if (lastStyleInserted.styleSheet) {
-            console.log('first case, replace text')
             lastStyleInserted.styleSheet.cssText = `${lastStyleInserted.styleSheet.cssText}\n/* ${id} */\n${css}`
           } else {
-            console.log('second case, append child text node')
             lastStyleInserted.appendChild(document.createTextNode(`/* ${id} */ ${css}`))
           }
         } else {
